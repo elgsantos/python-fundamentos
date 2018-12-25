985b68d5cf42e02dd32f05e8424a906bd23f6691
Eduardo Gomes <elgsantos@gmail.com>
2018-12-25 15:58:23 -0200

    Adicionado unique a emails

--- 14.1-pesquisar-email-pagina.py
+++ 14.1-pesquisar-email-pagina.py
@@ -1,11 +1,14 @@
 import re
 import requests
 
-requisicao = requests.get('https://www.unilasalle.edu.br/rj/')
+requisicao = requests.get('https://www.unilasalle.edu.br/rj/fale-conosco/')
 
 padrao = re.findall(r'[\w-]+@[\w-]+\.[\w\.-]+', requisicao.text) #r antes das aspas transforma em RAW string
 
 if padrao:
-    print(padrao)
+    conjunto = set()
+    for item in padrao:
+        conjunto.add(item)
+    print(conjunto)
 else:
     print('Padrão não encontrado')
